0590c19a5567b4e35388f740c7813e04628424e7
stankovictab <stankovictab@gmail.com>
2022-04-01 01:28:51 +0200

    new: Landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,35 @@
-<template>jo</template>
+<template>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,700;0,800;1,900&display=swap"
+		rel="stylesheet"
+	/>
+	<!--  -->
+	<div style="display: flex; justify-content: space-between">
+		<div class="landing-left">
+			<h1>Dislinkt</h1>
+			<h2>The place to connect.</h2>
+		</div>
+		<div class="landing-right">
+			<h3>
+				Start your professional career <br />by creating an account.
+			</h3>
+			<button style="margin: 30px 0px">Sign Up</button>
+			<p>Already have an account? <a href="/">Sign in</a></p>
+		</div>
+	</div>
+	<div style="margin-top: 220px"></div>
+	<div style="display: flex; justify-content: center">
+		<p class="withlove">
+			Made by
+			<a href="https://github.com/stankovictab/ftn-xws-dislinkt"
+				>Kvantaš</a
+			>
+			with 💙
+		</p>
+	</div>
+</template>
 
 <script>
 export default {
@@ -7,6 +38,3 @@ export default {
 };
 import "./style.css";
 </script>
-
-<style>
-</style>
