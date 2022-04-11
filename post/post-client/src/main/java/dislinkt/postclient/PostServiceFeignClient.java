04d2b9f0c5d5a4094dc8e6665a09bc274260aef4
stankovictab <stankovictab@gmail.com>
2022-04-11 21:04:43 +0200

    new: Docker compose rework & scripts

--- post/post-client/src/main/java/dislinkt/postclient/PostServiceFeignClient.java
+++ post/post-client/src/main/java/dislinkt/postclient/PostServiceFeignClient.java
@@ -1,13 +1,7 @@
 package dislinkt.postclient;
 
-import java.util.ArrayList;
-import java.util.Map;
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 // This Feign Client will be accessed by other microservices,
 // in order to communicate with this one
