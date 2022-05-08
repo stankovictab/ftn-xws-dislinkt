1db9daf61e29453375dfeb91482b931a2ee06c45
Mateja Miletic <mateja.miletic5@gmail.com>
2022-05-08 21:52:27 +0200

    fix user repo warning

--- user/user-service/src/main/java/dislinkt/userservice/Repository/UserRepository.java
+++ user/user-service/src/main/java/dislinkt/userservice/Repository/UserRepository.java
@@ -1,7 +1,6 @@
 package dislinkt.userservice.Repository;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
